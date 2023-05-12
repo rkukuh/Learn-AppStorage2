5479b6364e0c1e8eae21065a054731e75a6885e3
R. Kukuh <rkukuh@gmail.com>
2023-05-12 14:44:56 +0700

    Initial Commit

---
+++ AppStorage02/AppStorage02App.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorage02App.swift
+//  AppStorage02
+//
+//  Created by R. Kukuh on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorage02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStorage02/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppStorage02
+//
+//  Created by R. Kukuh on 12/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
